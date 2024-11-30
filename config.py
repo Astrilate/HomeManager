aeb9bd1d62bbdd48344863c57ece8e456d82b274
root <astrilate@gmail.com>
2024-11-30 20:30:29 +0800

    连接openGauss

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-SECRET_KEY = 'xxxxxx'
+from sqlalchemy.dialects.postgresql.base import PGDialect
 
-# 暂时使用 SQLite 内存数据库
-SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-# 如果需要持久化到文件，可以改成以下路径：
-# SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(os.getcwd(), "homebox.db")}'
+# 解决无法解析版本号字符串的报错
+PGDialect._get_server_version_info = lambda *args: (9, 2)
 
-SQLALCHEMY_TRACK_MODIFICATIONS = False  # 关闭追踪修改，提高性能
+# 可以临时使用 SQLite 内存数据库
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+# 连接到openGauss数据库
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://qcz1:Root123456@113.44.137.211:26000/qcz1'
+SQLALCHEMY_TRACK_MODIFICATIONS = False  # 不需要自动commit，关闭追踪修改，提高性能
