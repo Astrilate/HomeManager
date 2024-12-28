9e05458078bbd287ae71ab5038803727a9eff864
root <astrilate@gmail.com>
2024-12-28 22:04:06 +0800

    事后补充，加入了 MySQL 的连接方式

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 from sqlalchemy.dialects.postgresql.base import PGDialect
 
-# 解决无法解析版本号字符串的报错
-PGDialect._get_server_version_info = lambda *args: (9, 2)
-
-# 可以临时使用 SQLite 内存数据库
+# 连接到临时的 SQLite 内存数据库
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-# 连接到openGauss数据库
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://qcz1:Root123456@113.44.137.211:26000/qcz1'
+# 连接到远程的 openGauss 数据库
+# PGDialect._get_server_version_info = lambda *args: (9, 2) # openGauss 解决无法解析版本号字符串的报错
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://qcz1:Root123456@113.44.137.211:26000/qcz1'
+# 连接到本地的 MySQL 数据库
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@127.0.0.1:3306/homemanager'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False  # 不需要自动commit，关闭追踪修改，提高性能
 SECRET_KEY = '102201307QiuCongZhu'  # 用于生成JWT令牌的密钥
 UPLOAD_FOLDER_PHOTOS = 'static/uploads/photos/'
